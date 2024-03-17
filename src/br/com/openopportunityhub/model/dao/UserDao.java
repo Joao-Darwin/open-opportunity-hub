6b6109d268ab43a5f6ea8fe2ff7d0b21238b491c
João Darwin <joaodarwin.ip22@gmail.com>
2024-03-17 00:42:45 -0300

    feat: Criar base dos métodos presentes nos DAOs

--- src/br/com/openopportunityhub/model/dao/UserDao.java
+++ src/br/com/openopportunityhub/model/dao/UserDao.java
@@ -1,6 +1,9 @@
 package br.com.openopportunityhub.model.dao;
 
 import java.sql.Connection;
+import java.util.List;
+
+import br.com.openopportunityhub.model.entities.User;
 
 public class UserDao {
 
@@ -8,6 +11,26 @@ public class UserDao {
 	
 	public UserDao(Connection connection) {
 		this.connection = connection;
+	}
+	
+	public void create(User user) {
+		
+	}
+	
+	public User findById(int id) {
+		
+	}
+	
+	public List<User> findAll() {
+		
+	}
+	
+	public void update(int id, User user) {
+		
+	}
+	
+	public void delete(int id) {
+		
 	}
 	
 }
--- src/br/com/openopportunityhub/model/dao/VacancyDao.java
+++ src/br/com/openopportunityhub/model/dao/VacancyDao.java
@@ -1,11 +1,34 @@
 package br.com.openopportunityhub.model.dao;
 
 import java.sql.Connection;
+import java.util.List;
+
+import br.com.openopportunityhub.model.entities.Vacancy;
 
 public class VacancyDao {
 	private Connection connection;
 
 	public VacancyDao(Connection connection) {
 		this.connection = connection;
+	}
+
+	public void create(Vacancy user) {
+
+	}
+
+	public Vacancy findById(int id) {
+
+	}
+
+	public List<Vacancy> findAll() {
+
+	}
+
+	public void update(int id, Vacancy vacancy) {
+
+	}
+
+	public void delete(int id) {
+
 	}
 }
